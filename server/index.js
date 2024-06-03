c6d4be4d855f6d6700ea097c2e29d8c61a93cbe5
Istiak Ahmed <mimalahmed6219@gmail.com>
2024-06-03 21:55:30 +0600

    complete project

---
+++ server/index.js
@@ -0,0 +1,20 @@
+import cors from "cors";
+import * as dotenv from "dotenv";
+import express from "express";
+
+import dalleRoutes from "./routes/dalle.routes.js";
+
+dotenv.config();
+
+const app = express();
+app.use(cors());
+app.use(express.json({ limig: "50mb" }));
+console.log(`OpenAI API Key: ${process.env.OPENAI_API_KEY}`);
+
+app.use("/api/v1/dalle", dalleRoutes);
+
+app.get("/", (req, res) => {
+  res.status(200).json({ message: "Hello from DALL.E" });
+});
+
+app.listen(8080, () => console.log("Server has started on port 8080"));
